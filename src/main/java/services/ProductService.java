74f900d6a00b6eb3d2196c26ca474a79a356f136
NaeRae <naeimr4@gmail.com>
2022-01-31 16:18:16 +0330

    ProductService basic interface methods completed,

--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -1,32 +1,35 @@
 package services;
 
+import repositories.ProductsRepository;
 import things.shopRelated.Product;
 
 import java.sql.Connection;
 import java.util.List;
 
 public class ProductService extends Service<Product>{
+    ProductsRepository pr;
     public ProductService(Connection connection) {
         super(connection);
+        pr = new ProductsRepository(super.getConnection());
     }
 
     @Override
-    public Product find(Integer id) {
-        return null;
+    public Product find(Integer productId) {
+        return pr.read(productId);
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return pr.readAll();
     }
 
     @Override
     public Integer update(Product product) {
-        return null;
+        return pr.update(product);
     }
 
     @Override
-    public Integer delete(Integer id) {
-        return null;
+    public Integer delete(Integer productId) {
+        return pr.delete(productId);
     }
 }
